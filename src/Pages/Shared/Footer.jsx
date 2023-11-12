6ef26304bf808f180359c02441c59300afe70007
hirendranath <contact.hirendranath@gmail.com>
2023-11-12 13:27:15 +0600

    footer added

---
+++ src/Pages/Shared/Footer.jsx
@@ -0,0 +1,52 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer p-10 bg-base-200">
+                <nav>
+                    <header className="footer-title">Services</header>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <header className="footer-title">Company</header>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <header className="footer-title">Legal</header>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+                <form>
+                    <header className="footer-title">Newsletter</header>
+                    <fieldset className="form-control w-80">
+                        <label className="label">
+                            <span className="label-text">Enter your email address</span>
+                        </label>
+                        <div className="relative">
+                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
+                            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        </div>
+                    </fieldset>
+                </form>
+
+            </div>
+            <div className="text-center p-4 bg-base-300">
+                <p>
+                    Copyright © 2023 - All right reserved by <span className="link link-hover font-bold text-red-500">
+                        <a href="https://developerhiren.com/" target="_blank" rel="noopener noreferrer"> Developer Hiren</a>
+                    </span>
+                </p>
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
